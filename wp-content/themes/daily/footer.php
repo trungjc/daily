485bfc8b91192651186cbf66a5c1b8032ba0af61
trungjc <trungnguyenthanh70@gmail.com>
2014-10-17 13:51:32 +0700

    style home page desktop

--- wp-content/themes/daily/footer.php
+++ wp-content/themes/daily/footer.php
@@ -1,58 +1,62 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
-                            <div class="container">
-                               
-                                 
-                                <div class="follow-us-container">
-                                   <img src="<?php echo get_template_directory_uri(); ?>/img/food-trust.png" >     
-                                    <div class="clearfix">
-                                        <div class="pull-right follow-us">
-                                        <a href="" class="tiwtter">tiwtter</a>
-                                        <a href="" class="facebook">facebook</a>
-                                        </div>
-                                        Follow us on twitter or facebook to get our latest delivery updates
-                                    </div>
-                                    
-                                </div>
-                                <div class="footer-widget">
-                                    <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-3')) ?>
-                                </div>
+<!-- footer -->
+<footer class="footer" role="contentinfo">
+     <div class="container text-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/food-trust.png" >     
+        </div>
+    <div class="sub-footer">
+        <div class="container">
+                Follow us on twitter or facebook to get our latest delivery updates
+                <div class="follow-us">
+                    <a href="" class="tiwtter">tiwtter</a>
+                    <a href="" class="facebook">facebook</a>
+                </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-widget">
+                <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-3'))  ?>
+            </div>
+        </div>
+    </div>  
+</footer>
+<!-- /footer -->
 
-                              
-                            </div>
-				
+</div>
+<!-- /wrapper -->
 
-			</footer>
-			<!-- /footer -->
+<?php wp_footer(); ?>
 
-		</div>
-		<!-- /wrapper -->
-
-		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+<!-- analytics -->
 <script>
-jQuery(function($) {
-$('.navbar .dropdown').hover(function() {
-$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+    (function(f, i, r, e, s, h, l) {
+        i['GoogleAnalyticsObject'] = s;
+        f[s] = f[s] || function() {
+            (f[s].q = f[s].q || []).push(arguments)
+        }, f[s].l = 1 * new Date();
+        h = i.createElement(r),
+                l = i.getElementsByTagName(r)[0];
+        h.async = 1;
+        h.src = e;
+        l.parentNode.insertBefore(h, l)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+    ga('send', 'pageview');
+</script>
+<script>
+    jQuery(function($) {
+        $('.navbar .dropdown').hover(function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
 
-}, function() {
-$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+        }, function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
 
-});
+        });
 
-$('.navbar .dropdown > a').click(function(){  
-location.href = this.href;
-});
+        $('.navbar .dropdown > a').click(function() {
+            location.href = this.href;
+        });
 
-});</script>
-	</body>
+    });</script>
+</body>
 </html>
