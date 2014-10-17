a6b2d3959ed3e7e2c8c5ecc521197a06c5bf2ea3
trungjc <trungnguyenthanh70@gmail.com>
2014-10-17 18:12:59 +0700

    testimonial and stylesheet

--- wp-content/themes/daily/functions.php
+++ wp-content/themes/daily/functions.php
@@ -450,4 +450,19 @@ require_once('wp_bootstrap_navwalker.php');
 register_nav_menus( array(
 	'primary' => __( 'Primary Menu', 'THEMENAME' ),
 ) );
+function is_tree( $pid ) {      // $pid = The ID of the page we're looking for pages underneath
+    global $post;               // load details about this page
+
+    if ( is_page($pid) )
+        return true;            // we're at the page or at a sub page
+
+    $anc = get_post_ancestors( $post->ID );
+    foreach ( $anc as $ancestor ) {
+        if( is_page() && $ancestor == $pid ) {
+            return true;
+        }
+    }
+
+    return false;  // we arn't at the page, and the page is not an ancestor
+}
 ?>
